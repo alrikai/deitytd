2113a1cf23dd7663c775ac93d495571c2c644faf
Alrik <afirlortwo@gmail.com>
2019-04-13 23:33:18 -0700

    WIP: refactoring test arrangement, adding compound modifier tests

--- Testing/ModelTest/TestGameMechanics.cpp
+++ Testing/ModelTest/TestGameMechanics.cpp
@@ -7,10 +7,7 @@
 #include "util/TowerModifiers.hpp"
 #include "util/Types.hpp"
 
-#include "Common/shared/Player.hpp"
-#include "Common/shared/common_information.hpp"
-
-#include "Testing/fractal_util.hpp"
+#include "utils/Frontend.hpp"
 
 #include <chrono>
 #include <thread>
@@ -20,55 +17,8 @@
 #include <string>
 #include <vector>
 
-namespace TestStubs {
-// mock-up of the frontend
-template <typename BackendType> struct FrontStub {
-  using ModifierType = tower_properties;
-  using TowerEventQueueType = typename UserTowerEvents::EventQueueType<
-      UserTowerEvents::tower_event<BackendType>>::QType;
-  using GameInformationType =
-      GameInformation<CommonTowerInformation, TDPlayerInformation>;
-
-  void draw_maptiles(const int width, const int height) {}
-
-  void register_backend_eventqueue(ViewEvents *events) {}
-
-  void register_tower_eventqueue(TowerEventQueueType *tevt_queue) {
-    td_event_queue = tevt_queue;
-  }
-
-  void register_shared_info(std::shared_ptr<GameInformationType> s_info) {
-    shared_gamestate_info = s_info;
-  }
-
-  std::shared_ptr<GameInformationType> shared_gamestate_info;
-  TowerEventQueueType *td_event_queue;
-};
-
-template <typename TDType> void add_tower_displayinfo(TDType &td) {
-  using PixelType = uint8_t;
-  std::string mesh_filename{TDHelpers::get_basepath() +
-                            "/data/meshfractal3d.vtk"};
-
-  std::vector<std::vector<uint32_t>> polygon_mesh;
-  std::vector<std::vector<float>> polygon_points;
-  views_utils::add_mesh<PixelType>(polygon_mesh, polygon_points, mesh_filename);
-
-  const std::string tower_material{"Examples/Chrome"};
-  const std::string tower_name{"ViewTest"};
-  td->add_tower(std::move(polygon_mesh), std::move(polygon_points),
-                tower_material, tower_name);
-}
-
-// TODO: this should be part of the tower utils class -- this will load a yaml
-// file, and generate a tower_property_modifier object from the stats in the yaml
-// file.
-
-} // namespace TestStubs
-
 // breakpoint on failure:
 // gdb --args ./bin/GameMechanicsTest --gtest_break_on_failure
-
 namespace {
 class DTDBackendTest : public testing::Test {
 protected:
@@ -85,6 +35,8 @@ protected:
     mob_xcoord = 1;
     mob_ycoord = 1;
     tid = 0;
+    tmod_basepath = "/data/tests/towers/";
+    tower_basepath = TDHelpers::get_basepath() + tmod_basepath;
 
     for (size_t dmg_idx = 0; dmg_idx < tower_property_modifier::NUM_ELEM;
          dmg_idx++) {
@@ -135,6 +87,9 @@ protected:
   uint32_t tid;
   tower_properties expected_base_props;
   ModifierMapper tower_modifier_loader;
+
+  std::string tmod_basepath;
+  std::string tower_basepath;
 
   const std::string attack_id;
 };
@@ -197,8 +152,7 @@ TEST_F(DTDBackendTest, Basic_FlatDMG) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_flatED.yaml"};
+  const std::string tower_fpath{tower_basepath + "basic_flatED.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -252,8 +206,7 @@ TEST_F(DTDBackendTest, Basic_PctDMG) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_pctED.yaml"};
+  const std::string tower_fpath{tower_basepath + "basic_pctED.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -306,8 +259,8 @@ TEST_F(DTDBackendTest, Basic_flat_crit_multiplier) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_critamt.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_critamt.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -356,8 +309,8 @@ TEST_F(DTDBackendTest, Basic_critpct) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_critpct.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_critpct.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -409,8 +362,8 @@ TEST_F(DTDBackendTest, Basic_range) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_range.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_range.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -460,8 +413,8 @@ TEST_F(DTDBackendTest, Basic_spd) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_spd.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_spd.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -514,8 +467,8 @@ TEST_F(DTDBackendTest, Basic_type_FED) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_typeflatED.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_typeflatED.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -570,8 +523,8 @@ TEST_F(DTDBackendTest, Basic_type_PCTED) {
   assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
-  const std::string tower_fpath{TDHelpers::get_basepath() +
-                                "/data/tests/towers/basic_typepctED.yaml"};
+  const std::string tower_fpath{tower_basepath +
+                                "basic_typepctED.yaml"};
   tower_property_modifier tmod =
       tower_modifier_loader.parse_tower_config(tower_fpath);
 
@@ -602,6 +555,59 @@ TEST_F(DTDBackendTest, Basic_type_PCTED) {
   EXPECT_FLOAT_EQ(mob_stats.health, 993);
 }
 
+TEST_F (DTDBackendTest, Compound_F_ED) {
+  // create the basic fundamnetal tower
+  const int tier = 1;
+  auto td_backend = td->get_td_backend();
+  
+	//make the monster
+  const std::string mob_fpath { TDHelpers::get_basepath() + "/data/tests/monsters/basic_t0.yaml"};
+  auto mob = make_mob_from_cfg(mob_fpath);
+
+	//make the basic tower
+  td_backend->make_tower(tid, tier, tower_xcoord,tower_ycoord);
+  auto test_tower = td_backend->get_tower(tower_xcoord, tower_ycoord);
+  test_tower->set_target(mob);
+
+  std::vector<std::string> tmods {"basic_flatED.yaml", "basic_pctED.yaml"};
+  // load the tower modifiers, apply them to the base tower
+  tower_property_modifier tmod;
+  for (auto& str_mod : tmods) {
+    const std::string tower_fpath { tower_basepath + str_mod};
+    tower_property_modifier mod = tower_modifier_loader.parse_tower_config(tower_fpath);
+    tmod.merge(mod);
+  }
+	tower_properties expected_props = basic_attack_vals;
+  expected_props.apply_property_modifier(tmod);
+
+  test_tower->add_modifier(std::move(tmod));
+  //auto tdamage = tower->compute_attack_damage();
+  
+	test_tower->set_target(mob);
+  auto attack = test_tower->generate_attack(attack_id, 0);
+  auto attack_vals = attack->get_attack_attributes();
+  assert_tower_properties_almost_equals(attack_vals, expected_props);
+  tower_properties expected_handprops = expected_base_props;
+  for (size_t dmg_idx = 0; dmg_idx < tower_property_modifier::NUM_ELEM; dmg_idx++) {
+    expected_handprops.damage[dmg_idx].low += 10;
+    expected_handprops.damage[dmg_idx].high += 20;
+
+    expected_handprops.damage[dmg_idx].low *= (1.0 + 0.3);
+    expected_handprops.damage[dmg_idx].high *= (1.0 + 0.3);
+  }
+  assert_tower_properties_almost_equals(attack_vals, expected_handprops);
+
+  attack->set_target(Coordinate<float>(mob_xcoord, mob_ycoord));
+
+  std::list<std::weak_ptr<Monster>> moblist; 
+  moblist.emplace_back(mob);
+  compute_attackhit(moblist, std::move(attack));
+
+  //check the mob state, make sure the correct damage was done
+  MonsterStats mob_stats = mob->get_attributes();
+  EXPECT_FLOAT_EQ(mob_stats.health, 94.2195969);
+}
+
 #if 0
 TEST_F (DTDBackendTest, Basic_) {
   // create the basic fundamnetal tower
@@ -616,13 +622,6 @@ TEST_F (DTDBackendTest, Basic_) {
   td_backend->make_tower(tid, tier, tower_xcoord,tower_ycoord);
   auto test_tower = td_backend->get_tower(tower_xcoord, tower_ycoord);
   test_tower->set_target(mob);
-
-	//make the (unmodified) basic tower attack. 
-  //this is the default (fundamental) tower stats, without any boosts
-  const std::string basic_attack_id {"pct_atk"};
-  auto basic_attack = test_tower->generate_attack(basic_attack_id, 0);
-  auto basic_attack_vals = basic_attack->get_attack_attributes();
-  assert_tower_properties_almost_equals(basic_attack_vals, expected_base_props);
 
   // load the tower modifiers, apply them to the base tower
   const std::string tower_fpath { TDHelpers::get_basepath() + "/data/tests/towers/basic_pctED.yaml"};
